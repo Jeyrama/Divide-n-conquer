3f9b42db7a5a249e1f2d64249fb3f4e1d0d5a85c
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2024-10-04 20:47:18 +0200

    Update Divide-n-conquer.js

--- Divide-n-conquer.js
+++ Divide-n-conquer.js
@@ -9,3 +9,17 @@ Return as a number.
 
 
 // Solution
+
+function divCon(x){
+  let numValue = 0;
+  let strValue = 0;
+  
+  for(let i = 0; i < x.length; i++) {
+    if(typeof(x[i]) === 'string' ) {
+      strValue += parseFloat(x[i]);
+    } else if(typeof(x[i]) === 'number' ) {
+      numValue += x[i];
+    }
+  }
+  return numValue - strValue;
+}
